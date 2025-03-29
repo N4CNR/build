71b68f9ace654e5f412c2ebf2a3ca88998b8943f
N4CNR <n4cnr.ham@gmail.com>
2025-03-29 10:51:02 -0400

    updating builder for xfce

--- config/desktop/sid/appgroups/lightdm/armbian/create_desktop_package.sh
+++ config/desktop/sid/appgroups/lightdm/armbian/create_desktop_package.sh
@@ -1,7 +1,2 @@
-# install sdddm greeter
-#cp -R "${SRC}"/packages/blobs/desktop/sddm "${destination}"/etc/armbian
-
-# install default desktop settings
-mkdir -p "${destination}"/etc/xdg
-cp -R "${SRC}"/packages/blobs/desktop/debian-xdg/xdg-xfce/. "${destination}"/etc/xdg
-
+# install lightdm greeter
+cp -R "${SRC}"/packages/blobs/desktop/lightdm "${destination}"/etc/armbian
