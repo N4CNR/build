63aa21cec32cb57176d8d61c93aee7266d6841f6
N4CNR <n4cnr.ham@gmail.com>
2025-04-03 10:44:35 -0400

    change names
    xfceto xfce-labwc and add xfce-wayfire

---
+++ config/desktop/sid/environments/xfce-labwc/armbian/create_desktop_package.sh
@@ -0,0 +1,23 @@
+# install sdddm greeter theme
+#cp -R "${SRC}"/packages/blobs/desktop/sddm "${destination}"/etc/armbian
+
+# install default desktop settings
+mkdir -p "${destination}"/etc/armbian
+cp -r "${SRC}"/packages/blobs/desktop/debian-xdg/xdg-xfce "${destination}"/etc/armbian
+cp -r "${SRC}"/packages/blobs/desktop/debian-xdg/xdg-common "${destination}"/etc/armbian
+
+#install cinnamon desktop bar icons
+mkdir -p "${destination}"/usr/share/icons/armbian
+cp "${SRC}"/packages/blobs/desktop/desktop-icons/*.png "${destination}"/usr/share/icons/armbian
+
+# install wallpapers
+mkdir -p "${destination}"/usr/share/backgrounds/armbian/
+cp "${SRC}"/packages/blobs/desktop/desktop-wallpapers/*.jpg "${destination}"/usr/share/backgrounds/armbian
+
+# install wallpapers
+mkdir -p "${destination}"/usr/share/backgrounds/armbian-lightdm/
+cp "${SRC}"/packages/blobs/desktop/lightdm-wallpapers/*.jpg "${destination}"/usr/share/backgrounds/armbian-lightdm
+
+# install logo for login screen
+mkdir -p "${destination}"/usr/share/pixmaps/armbian
+cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
