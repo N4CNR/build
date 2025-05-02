c8065a0142ebd833d16be5c4d0008763fd860807
N4CNR <n4cnr.ham@gmail.com>
2025-05-02 12:11:47 -0400

    Add Desktops
    add Trixie and Noble dekstops

---
+++ config/desktop/noble/environments/kde-neon/armbian/create_desktop_package.sh
@@ -0,0 +1,24 @@
+# install default desktop settings
+mkdir -pv "${destination}"/etc/skel
+cp -Rv "${SRC}"/packages/blobs/desktop/skel.common/. "${destination}"/etc/skel
+
+# install wallpapers
+mkdir -pv "${destination}"/usr/share/backgrounds/armbian/
+cp -v "${SRC}"/packages/blobs/desktop/desktop-wallpapers/*.jpg "${destination}"/usr/share/backgrounds/armbian
+
+# install logo for login screen
+mkdir -pv "${destination}"/usr/share/pixmaps/armbian
+cp -v "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
+
+# set default wallpaper
+#echo "
+#dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
+#var Desktops = desktops();
+#for (i=0;i<Desktops.length;i++) {
+#        d = Desktops[i];
+#        d.wallpaperPlugin = \"org.kde.image\";
+#        d.currentConfigGroup = Array(\"Wallpaper\",
+#                                    \"org.kde.image\",
+#                                    \"General\");
+#        d.writeConfig(\"Image\", \"file:///usr/share/backgrounds/armbian/armbian03-Dre0x-Minum-dark-3840x2160.jpg\");
+#}'" > "${destination}"/usr/share/backgrounds/armbian/set-armbian-wallpaper.sh
