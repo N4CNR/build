400b7c53afd53c5b82fc78668a664004c3eb3af9
Richard Neese <n4cnr.ham@gmail.com>
2025-05-15 15:16:07 -0400

    adding desktops to plucky
    xfce-labwc and kde-neon stable/testing

---
+++ config/desktop/plucky/environments/kde-neon-stable/armbian/create_desktop_package.sh
@@ -0,0 +1,28 @@
+# install default desktop settings
+mkdir -pv "${destination}"/etc/skel
+cp -Rv "${SRC}"/packages/blobs/desktop/skel_common/. "${destination}"/etc/skel
+
+# install wallpapers
+mkdir -pv "${destination}"/usr/share/wallpapers/armbian/
+cp -v "${SRC}"/packages/blobs/desktop/desktop-wallpapers/*.jpg "${destination}"/usr/share/wallpapers/armbian
+
+# install wallpapers
+mkdir -p "${destination}"/usr/share/armbian-sddm/
+cp "${SRC}"/packages/blobs/desktop/lightdm-wallpapers/*.jpg "${destination}"/usr/share/armbian-sddm
+
+# install logo 
+mkdir -pv "${destination}"/usr/share/pixmaps/armbian
+cp -v "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
+
+# set default wallpaper
+#echo "
+#dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
+#var Desktops = desktops();
+#for (i=0;i<Desktops.length;i++) {
+#        d = Desktops[i];
+#        d.wallpaperPlugin = \"org.kde.image\";
+#        d.currentConfigGroup = Array(\"Wallpaper\",
+#                                    \"org.kde.image\",
+#                                    \"General\");
+#        d.writeConfig(\"Image\", \"file:///usr/share/wallpapers/armbian/armbian03-Dre0x-Minum-dark-3840x2160.jpg\");
+#}'" > "${destination}"/usr/share/wallpapers/armbian/set-armbian-wallpaper.sh
