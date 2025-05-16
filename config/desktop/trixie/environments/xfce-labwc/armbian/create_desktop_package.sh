e29e2bd8a9bf16539e527c1f77fef5ca43e9f87e
Richard Neese <n4cnr.ham@gmail.com>
2025-05-16 19:29:13 -0400

    updating desktops

--- config/desktop/trixie/environments/xfce-labwc/armbian/create_desktop_package.sh
+++ config/desktop/trixie/environments/xfce-labwc/armbian/create_desktop_package.sh
@@ -14,15 +14,3 @@ cp "${SRC}"/packages/blobs/desktop/lightdm-wallpapers/*.jpg "${destination}"/usr
 mkdir -pv "${destination}"/usr/share/pixmaps/armbian
 cp -v "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
 
-# set default wallpaper
-#echo "
-#dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
-#var Desktops = desktops();
-#for (i=0;i<Desktops.length;i++) {
-#        d = Desktops[i];
-#        d.wallpaperPlugin = \"org.kde.image\";
-#        d.currentConfigGroup = Array(\"Wallpaper\",
-#                                    \"org.kde.image\",
-#                                    \"General\");
-#        d.writeConfig(\"Image\", \"file:///usr/share/wallpapers/armbian/armbian03-Dre0x-Minum-dark-3840x2160.jpg\");
-#}'" > "${destination}"/usr/share/wallpapers/armbian/set-armbian-wallpaper.sh
