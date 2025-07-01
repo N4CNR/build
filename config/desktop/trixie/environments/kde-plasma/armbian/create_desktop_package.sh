5da7f0e0dd3d7880c5ee6aa641539ed0a4e9ca23
N4CNR <n4cnr.ham@gmail.com>
2025-07-01 12:08:02 -0400

    cleaning out armbian crap to go more debian native

--- config/desktop/trixie/environments/kde-plasma/armbian/create_desktop_package.sh
+++ config/desktop/trixie/environments/kde-plasma/armbian/create_desktop_package.sh
@@ -2,18 +2,6 @@
 mkdir -pv "${destination}"/etc/skel
 cp -Rv "${SRC}"/packages/blobs/desktop/skel_common/. "${destination}"/etc/skel
 
-# install wallpapers
-mkdir -pv "${destination}"/usr/share/wallpapers/armbian/
-cp -v "${SRC}"/packages/blobs/desktop/desktop-wallpapers/*.jpg "${destination}"/usr/share/wallpapers/armbian
-
-# install sddm wallpapers
-mkdir -p "${destination}"/usr/share/sddm-armbian/
-cp "${SRC}"/packages/blobs/desktop/sddm-wallpapers/*.jpg "${destination}"/usr/share/sddm-armbian
-
-# install logo for login screen
-mkdir -pv "${destination}"/usr/share/pixmaps/armbian
-cp -v "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
-
 # set default wallpaper
 #echo "
 #dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
